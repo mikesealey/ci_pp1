178462efcee1998750bba181564814535b7e4b75
Mike Sealey <mikesealey@hotmail.com>
2024-09-25 20:01:34 +0000

    adds styling and structure to nav and footer

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -2,11 +2,21 @@ let myFunction = () => {
     console.log("function called!")
 }
 
-window.addEventListener("scroll", () => {
-    console.log("Scrolled!");
-    let nav = document.getElementById("nav")
-
-    nav.classList.contains("smaller") ? "" : nav.classList.add("smaller")
-});
+// window.addEventListener("scroll", () => {
+//     console.log(window.scrollY)
+//     let nav = document.getElementById("nav")
+//     let icon = document.getElementById("icon")
+//     if (window.scrollY > 100 & !nav.classList.contains("smaller") & !icon.classList.contains("smaller")) {
+//         nav.classList.add("smaller")
+//         nav.classList.add("smaller")
+//     } else {
+//         nav.classList.remove("smaller")
+//         nav.classList.remove("smaller")
+//     }
+    
+    
+// });
 
 // When scrolling, if the nav does not currently contain the class "smaller", add the class "smaller"
+
+// Find a way to shrink this by a 
