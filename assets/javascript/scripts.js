705037a01d29d9c289c3aa527a730cd6047ecb65
Mike Sealey <mikesealey@hotmail.com>
2024-10-02 19:53:14 +0000

    removes old resizing logic, begins to replace

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,21 +1,12 @@
-let myFunction = () => {
-    console.log("function called!")
-}
-
-const navigation = document.getElementById("nav");
-
 window.onscroll = function() {
-    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    document.getElementById("nav").classList.add("nav-smaller")
 
-    // Math.max takes arguments and returns whichever is the highest 
-    // This prevents the size% going lower than 15%
-    const newWidthPercentage = Math.max(15, 100 - (85 * scrollY / 700)); // Shrinks from 100% to 15%
-
-    navigation.style.width = Math.round(newWidthPercentage) + "%";
-    console.log(newWidthPercentage, scrollY)
+    if (window.scrollY === 0) {
+        document.getElementById("nav").classList.remove("nav-smaller")
+    }
   };
 
 addEventListener("load", (event) => {
-    console.log("Page loaded!")
+    document.getElementById("nav").classList.remove("nav-smaller")
 })
 
