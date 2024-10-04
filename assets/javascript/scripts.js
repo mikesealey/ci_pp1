d28be2d0ecbbe74212eb0097d7a4c6ed91451685
Mike Sealey <mikesealey@hotmail.com>
2024-10-04 18:32:54 +0000

    adjusts nav-logo transition

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,12 +1,19 @@
 window.onscroll = function() {
     document.getElementById("nav").classList.add("nav-smaller")
-
+    document.getElementById("nav-links").classList.add("nav-links-smaller")
+    document.getElementById("page-content").classList.add("page-content-higher")
     if (window.scrollY === 0) {
         document.getElementById("nav").classList.remove("nav-smaller")
+        document.getElementById("nav-links").classList.remove("nav-links-smaller")
+        document.getElementById("page-content").classList.remove("page-content-higher")
     }
   };
 
 addEventListener("load", (event) => {
     document.getElementById("nav").classList.remove("nav-smaller")
+    document.getElementById("nav-links").classList.remove("nav-links-smaller")
+    document.getElementById("page-content").classList.remove("page-content-higher")
+
+
 })
 
